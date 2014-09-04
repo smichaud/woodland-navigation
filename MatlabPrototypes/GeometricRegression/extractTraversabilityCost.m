7ebfcffab2b687d93bf0c241d18eb4940711c975
smichaud <smichaud@smichaud-Z68A-D3H-B3.(none)>
2014-09-04 15:28:04 -0400

    Working on the traversability cost extraction

--- MatlabPrototypes/GeometricRegression/extractTraversabilityCost.m
+++ MatlabPrototypes/GeometricRegression/extractTraversabilityCost.m
@@ -1,2 +1,20 @@
-approxCurrentStopped = 0.5900;
-approxCurrentStartPeak = 6.0;
+standbyCurrent = 0.5900; % normally
+startPeakCurrent = 6.0; % both should be over that value
+
+robotSpeed = 0.3; % m/s
+distanceBeforeObstacles = 2; % meters
+delayBeforeTraversability = distanceBeforeObstacles/robotSpeed;
+traversabilityStartTime =  
+
+for i=1:nbOfSamples
+%     currents = dataset(i).rawCurrents;
+%     firstMoveIndex = find(currents(1,:) > startPeakCurrent && currents(1,:) > startPeakCurrent);
+%     
+%     clf;
+%     hold on;
+%     plot(dataset(i).rawCurrents(:,1), dataset(i).rawCurrents(:,2), 'b');
+%     plot(dataset(i).rawCurrents(:,1), dataset(i).rawCurrents(:,3), 'r');
+%     line([firstMoveIndex,firstMoveIndex],ylim, 'y');
+%     uiwait;
+end
+
