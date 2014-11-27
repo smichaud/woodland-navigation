00dab7eebc5a862e899cc519cc24aed60a69dd93
smichaud <smichaud@smichaud-Z68A-D3H-B3.(none)>
2014-11-27 12:18:50 -0500

    Renamed some directories using camelCase and added the first clustersLabeling prototype

---
+++ MatlabPrototypes/GeometricRegression/clustersLabeling/mainPrototyping.m
@@ -0,0 +1,29 @@
+nbOfSamples = length(dataset)
+
+dftVectors = zeros(nbOfSamples, length(dataset(1).dftIMU(:,1))); 
+for i = 1:nbOfSamples
+    dftVectors(i,:) = dataset(i).dftIMU(:,6)';
+   distanceMatrix = squareform(pdist(dftVectors, 'euclidean')); 
+end
+
+for i = 1:nbOfSamples
+    [vector orderedIndexes] = sort(distanceMatrix(i,:));
+    
+    figure('Name', 'Nearest Neighbor (IMU)', 'units','normalized',...
+        'outerposition',[0 0 1 1]);
+    subplot(2,2,1);
+    imshow(dataset(i).image);
+    title('Example')
+    subplot(2,2,2);
+    imshow(dataset(orderedIndexes(2)).image);
+    title(sprintf('1st nearest (%f)', vector(2)));
+    subplot(2,2,3);
+    imshow(dataset(orderedIndexes(3)).image);
+    title(sprintf('2nd nearest (%f)', vector(3)));
+    subplot(2,2,4);
+    imshow(dataset(orderedIndexes(4)).image);
+    title(sprintf('3rd nearest (%f)', vector(4)));
+    
+    waitforbuttonpress;
+    close all;
+end
