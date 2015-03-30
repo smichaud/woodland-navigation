d0e93564c6f1a15333f29fde52cbb3fd4133c697
edminster <edminster@edminster-G74Sx>
2015-03-30 10:26:30 -0400

    Changed the main to test list of words

--- Matlab/simulation/mainSimulation.m
+++ Matlab/simulation/mainSimulation.m
@@ -4,31 +4,40 @@ clc
 
 datasetDir = '../../../WoodlandNavigationData/SimulatedPointClouds/';
 
-isLoadedFromCsv = false;
+isLoadedFromCsv = true;
 if isLoadedFromCsv
     disp('Loading point couds from csv files...');
     dataset = loadSimulationDataset(datasetDir);
     save([datasetDir 'dataset.mat'], 'dataset');
-    
-    disp('Creating the points count voxel grid for all samples')
-    voxelSize = 0.1;
-    areaOfInterest = [-1,1 ; -1,1 ; -1,1];
-    dataset = buildPointsCountVoxelGrids(...
-        dataset, voxelSize, areaOfInterest);
 else
+    disp('Loading dataset from dataset.mat...');
     load([datasetDir 'dataset.mat']);
-    
-    isDatasetFiltered = true;
-    if isDatasetFiltered
-        samplesCount = length(dataset);
-        indexToRemove = [];
-        for i = 1:samplesCount
-            if dataset(i).isNoisy == true || i > 30
-                indexToRemove(end+1) = i;
-            end
+end
+
+isDatasetFiltered = true;
+if isDatasetFiltered
+    disp('Remove noisy example and extra samples...');
+    samplesCount = length(dataset);
+    indexToRemove = [];
+    for i = 1:samplesCount
+        if dataset(i).isNoisy == true
+            indexToRemove(end+1) = i;
         end
-        dataset(indexToRemove) = []  
+    end
+    dataset(indexToRemove) = [];
+    
+    if length(dataset) > 15
+        dataset(16:end) = [];
     end
 end
 
+disp('Creating the points count voxel grid for all samples')
+voxelSize = 0.1;
+areaOfInterest = [-1,1 ; -1,1 ; -1,1];
+dataset = buildPointsCountVoxelGrids(...
+    dataset, voxelSize, areaOfInterest);
+
+
 disp('Creating a list of all existing words (aka ratioGrids/features)')
+wordGridSize = 3;
+[dataset completeWordList] = buildWordLists(dataset, wordGridSize);
